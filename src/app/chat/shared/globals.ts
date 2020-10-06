a257cea75270af3c3d376af996a211e46d454c3e
iMac <imac@MacBook-Pro-de-macbookpro.local>
2020-10-06 20:18:12 +0200

    panamfoto

--- src/app/chat/shared/globals.ts
+++ src/app/chat/shared/globals.ts
@@ -5,9 +5,9 @@ export class GlobalService {
     
    //public static API_ENDPOINT='api-widget/';
     //public static  SOCKET_ENDPOINT = 'http://localhost:3000/'; 
-    public static NM_COOKIE='__sexpe__';
-    public static TEXTO_CAB='Extremadura Trabaja';
-    public static CONTACT_WAS='+34628653025';
+    public static NM_COOKIE='__bancona_chat__';
+    public static TEXTO_CAB='Banco Nacional';
+    public static CONTACT_WAS='+50765095151';
    // public static CONTACT_TEL='@Hygeia112_bot';
    
     //public static AVATAR_CAB='/assets/bankblanco.png';
@@ -18,7 +18,7 @@ export class GlobalService {
     public static AVATAR_CHAT= '/assets/logochat_red.svg';
     public static ICON_CANCEL= '/assets/cancel.png';
     //public static TXT_INICIAL='Hola, soy Eva de Adiper.  ¿Cómo puedo ayudarle?  Le agradecería que me facilite un nickname para dirigirme más fácilmente a usted.';
-    public static TXT_INICIAL="Un gusto saludarle, hablar conmigo es muy sencillo, escriba Hola y siga mis instrucciones. Es muy fácil !"; 
+    public static TXT_INICIAL="Bienvenido(a) a Banco Nacional de Panamá. ¡Soy Ariel, su Asistente Virtual! es un placer atenderle! "; 
     public static API_ENDPOINT='https://node1.smallshi.com/api-widget/';
     public static  SOCKET_ENDPOINT = 'https://node1.smallshi.com:1442/';
     //public static API_ENDPOINT='https://49544f13.ngrok.io/api-widget/';
