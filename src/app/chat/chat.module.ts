c15df4e14fbf5a054acf238697267242a0a7e3ec
Cristian Motta <cdmottah@gmail.com>
2020-11-11 10:48:59 -0500

    se elimina el componente chat-options que estaba en des uso

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -6,20 +6,27 @@ import { ChatInputComponent } from './chat-input/chat-input.component'
 import { ChatPreguntasComponent } from './chat-preguntas/chat-preguntas.component'
 import { NewTarjetaComponent } from './new-tarjeta/new-tarjeta.component'
 import { ChatConfigComponent } from './chat-config/chat-config.component'
-import { SocketService } from './shared/services/socket.service';
-import { AngularDraggableModule } from 'angular2-draggable';
-import { CookieService } from 'ngx-cookie-service';
-import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
-import { SanitizeHtmlPipe } from './pipe/sanitize-html.pipe';
-import { ChatOptionsComponent } from './chat-options/chat-options/chat-options.component';
+import { SocketService } from './shared/services/socket.service'
+import { AngularDraggableModule } from 'angular2-draggable'
+import { CookieService } from 'ngx-cookie-service'
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2'
+import { SanitizeHtmlPipe } from './pipe/sanitize-html.pipe'
 import { ChatAdjuntosComponent } from './chat-adjuntos/chat-adjuntos.component'
 
 @NgModule({
-  imports: [CommonModule,AngularDraggableModule,SweetAlert2Module],
-  declarations: [ChatAvatarComponent, ChatWidgetComponent, ChatInputComponent, ChatConfigComponent,ChatPreguntasComponent,SanitizeHtmlPipe,NewTarjetaComponent, ChatOptionsComponent, ChatAdjuntosComponent],
-  providers: [SocketService,CookieService,SanitizeHtmlPipe],
+  imports: [CommonModule, AngularDraggableModule, SweetAlert2Module],
+  declarations: [
+    ChatAvatarComponent,
+    ChatWidgetComponent,
+    ChatInputComponent,
+    ChatConfigComponent,
+    ChatPreguntasComponent,
+    SanitizeHtmlPipe,
+    NewTarjetaComponent,
+    ChatAdjuntosComponent,
+  ],
+  providers: [SocketService, CookieService, SanitizeHtmlPipe],
   exports: [ChatWidgetComponent, ChatConfigComponent],
   entryComponents: [ChatWidgetComponent, ChatConfigComponent],
-}) 
+})
 export class ChatModule {}
-  
