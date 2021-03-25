94158a5ccf25ba80bfd1d367fb37dd4d81a41de9
Cristian Motta <cristian.motta@silice.si>
2021-03-25 12:18:08 -0500

    se agrega al app module rutas y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,35 @@ import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
+import { RouterModule, Routes } from '@angular/router';
 import { ElementModule } from './element.module'
-import { AppComponent } from './app.component'
 import { ConfigFactory, ConfigService, TOKEN } from './chat/shared/services/config'
 import { HttpModule } from '@angular/http'
 import { NgxLinkifyjsModule } from 'ngx-linkifyjs'
 import { CookieService } from 'ngx-cookie-service'
-
+import { EncsessionService } from './chat/shared/helpers/encsession.service';
 import { NgxWebstorageModule } from 'ngx-webstorage'
 
-import { EncsessionService } from './chat/shared/helpers/encsession.service';
-import { EncabezadosComponent } from './web/encabezados/encabezados.component';
-import { WebComponent } from './web/web/web.component';
+
+import { AppComponent } from './app.component'
 import { BlogComponent } from './web/pages/blog/blog.component';
-import { CasosComponent } from './web/pages/casos/casos.component';
 import { CasosDeExitoComponent } from './web/pages/casos-de-exito/casos-de-exito.component';
+import { CasosComponent } from './web/pages/casos/casos.component';
+import { CaseStudiesComponent } from "./web/components/case-studies/case-studies.component";
+import { EncabezadosComponent } from './web/encabezados/encabezados.component';
 import { ExperienciaComponent } from './web/pages/experiencia/experiencia.component';
+import { EquipoComponent } from './web/pages/nosotros/equipo/equipo.component'
+import { HomeComponent } from './web/pages/home/home.component'
+import { HeaderComponent } from './web/components/header/header.component';
 import { LaboratorioComponent } from './web/pages/laboratorio/laboratorio.component';
 import { NosotrosComponent } from './web/pages/nosotros/nosotros.component';
 import { NoticiasComponent } from './web/pages/noticias/noticias.component';
-import { SolucionesComponent } from './web/pages/soluciones/soluciones.component';
-import { HeaderComponent } from './web/components/header/header.component';
 import { NavComponent } from './web/components/header/nav/nav.component';
+import { SolucionesComponent } from './web/pages/soluciones/soluciones.component';
 import { SliderComponent } from './web/components/principalSlider/slider/slider.component'
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './web/pages/home/home.component'
-import { CaseStudiesComponent } from "./web/components/case-studies/case-studies.component";
+import { TrayectoriaComponent } from './web/pages/nosotros/trayectoria/trayectoria.component'
+import { WebComponent } from './web/web/web.component';
+import { TimelineComponent } from './web/components/timeline/timeline.component';
 
 
 
@@ -36,8 +39,8 @@ const routes: Routes = [
     path: 'home', component: WebComponent,
     children: [
       { path: 'nosotros', component: NosotrosComponent, },
-      { path: 'trayectoria', component:NosotrosComponent},
-      // { path: 'equipo', component:TrayectoriaComponent},
+      { path: 'trayectoria', component:TrayectoriaComponent},
+      { path: 'equipo', component:EquipoComponent},
       { path: 'laboratorio', component: LaboratorioComponent },
       { path: 'soluciones', component: SolucionesComponent },
       { path: 'casos', component: CasosComponent },
@@ -63,21 +66,24 @@ const routes: Routes = [
     NgxWebstorageModule.forRoot()
   ],
   declarations: [AppComponent,
-    EncabezadosComponent,
-    WebComponent,
     BlogComponent,
     CasosComponent,
     CasosDeExitoComponent,
+    CaseStudiesComponent,
+    EncabezadosComponent,
+    EquipoComponent,
     ExperienciaComponent,
+    HeaderComponent,
+    HomeComponent,
     LaboratorioComponent,
     NosotrosComponent,
     NoticiasComponent,
-    SolucionesComponent,
-    HeaderComponent,
     NavComponent,
+    SolucionesComponent,
     SliderComponent,
-    CaseStudiesComponent,
-    HomeComponent
+    TrayectoriaComponent,
+    TimelineComponent,
+    WebComponent,
   ],
 
   providers: [
