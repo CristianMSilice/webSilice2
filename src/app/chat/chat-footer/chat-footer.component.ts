5a5688f16c823f3cd41b303f95e96cab3e0d7b21
Cristian Ardila <cristian.dah@gmail.com>
2021-04-21 15:23:52 -0500

    se agrega en el footer del chat los links a redes sociales!

--- src/app/chat/chat-footer/chat-footer.component.ts
+++ src/app/chat/chat-footer/chat-footer.component.ts
@@ -20,6 +20,13 @@ export class ChatFooterComponent implements OnInit {
   videos= [
   '8KHklcgn0xg','Jegv-Ht2Qeo','LQYyau47oy4','TAWXA-ccj7I','wzraTerfxLI','tJSu7nrf8AM'
   ]
+  links= [
+    {text:'Facebook', url:'https://www.facebook.com/sisilice/', texturl:'www.facebook.com/silice', image:'facebook.jpg'},
+    {text:'Linkedin', url:'https://www.linkedin.com/company/si-silice', texturl:'www.linkedin.com/silice', image:'linkedin.jpg'},
+    {text:'Instagram', url:'https://www.instagram.com/silicesi/', texturl:'www.instagram.com/Siice', image:'instagram.jpg'},
+    {text:'Twitter', url:'https://twitter.com/Si_Silice', texturl:'www.twitter.com/Si_Silice', image:'twitter.jpg'},
+    {text:'Youtube', url:'https://www.youtube.com/channel/UCI-ZhoL4V3uZorOJZD849dg', texturl:'www.youtube.com/Siice', image:'youtube.jpg'},
+  ]
 
   activeChatSetting(event:MouseEvent,active) {
     (this.active == active)
