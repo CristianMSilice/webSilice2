e7474ad318a45d7fd431697762608779de28500b
Cristian Ardila <cristian.dah@gmail.com>
2021-04-26 23:56:02 -0500

    se agrega nuevo estilo al detalle del pais

---
+++ src/app/web/components/mapa-mundi/country-model.ts
@@ -0,0 +1,13 @@
+export interface countryModel{
+    name:string;
+    svgName:string;
+    photo:string;
+    proyects:Array<proyect>
+    choords: {left:any , top:any}
+    date:string;
+}
+interface proyect {
+    name:string;
+    resume:string;
+    date:string;
+}
--- src/app/web/components/mapa-mundi/mapa-mundi.component.ts
+++ src/app/web/components/mapa-mundi/mapa-mundi.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 import { ChildActivationEnd } from '@angular/router';
+import { countryModel } from './country-model';
 
 @Component({
   selector: 'mapa-mundi',
@@ -12,44 +13,39 @@ export class MapaMundiComponent implements OnInit {
   constructor(private _sanitizer:DomSanitizer) { }
   countries;
   selectedCountry=undefined;
-  sedes = [
+  sedes: Array <countryModel>= [
     {
-      name: 'panama', text: 'Panamá', image: 'flag-panama.png', link: 'nosotros/sedes/panama',
-      team: { userimage: 'equipo-jonatan.jpg', userName: 'Jonatan' },
-      contry: [
-        { item: '+10 años', text: 'proyectos de transformación Digital' },
-        { item: '+10', text: 'años proyectos de transformación Digital Implementación proyectos tractores de cambio en Entidades Gubernamentales y Sector privado' },
-        { item: '+15', text: 'Proyectos de Omnicanalidad con servicios a Nivel Nacional' }
+      svgName: 'panama', name: 'Panamá', photo: 'colombia.jpg', 
+      date:'',
+      proyects:[
+        {name:'',resume:'',date:''}
       ],
        choords: {left:undefined ,top:undefined}
     },
-      {
-        name: 'colombia', text: 'Colombia', image: 'flag-colombia.png', link: 'nosotros/sedes/colombia',
-        team: { userimage: 'equipo-alvaro.jpg', userName: 'Alvaro' },
-        contry: [
-          { item: '+ de 300', text: 'mil pacientes Covid atendidos' },
-          { item: '+ 800', text: 'mil mensajes de Whatsapp' },                  
-          { item: '+ 150', text: 'mil llamadas robotizadas' }                  
-        ],
-        choords: {left: undefined ,top:undefined}
-      },
-      {
-        name: 'spain', text: 'España', image: 'flag-spain.png', link: 'nosotros/sedes/espana',
-        team: { userimage: 'equipo-juan.jpg', userName: 'Jonatan' },
-        contry: [
-          { item: '+ de 20', text: 'años de trayectoria' },
-          { item: '+ de 10', text: 'proyectos de transformación digital a través de la omicanalidad en organizaciones públicas y privadas.' },
-          { item: '1er', text: ' Laboratorio de innovación, investigación y desarrollo SILICE' }
-        ],
-        choords: {left:undefined ,top:undefined}
-      }
-    
+    {
+      svgName: 'colombia', name: 'Colombia', photo: 'colombia.jpg', 
+      date:'',
+      proyects:[
+        {name:'',resume:'',date:''}
+      ],
+       choords: {left:undefined ,top:undefined}
+    },
+    {
+      svgName: 'spain', name: 'España', photo: 'colombia.jpg', 
+      date:'Enero 20, 2001',
+      proyects:[
+        {name:'SIA',resume:'Instalación de PAU en españa',date:'2005'},
+        {name:'SMALLSHI',resume:'resolviendo la necesidad de',date:'2012'},
+        {name:'OMNITURNO',resume:'se instaló en x ciudad de',date:'2020'}
+      ],
+       choords: {left:undefined ,top:undefined}
+    },   
   ]
   changeCanvas=false;
   ngOnInit() {
     this.addCountries()
     setTimeout(()=>{
-      if(this.selectedCountry == undefined)this.selectedCountry = this.sedes[Math.floor(Math.random()*(this.sedes.length))].name;
+      if(this.selectedCountry == undefined)this.selectedCountry = this.sedes[Math.floor(Math.random()*(this.sedes.length))].svgName;
     },5000);
   }
 
@@ -75,6 +71,7 @@ export class MapaMundiComponent implements OnInit {
   }
 
   newSizeDotsContry(child) {
+    console.log(child)
     let web = document.querySelector('web');
     let name = child.getAttribute('title').toLowerCase()
     let size = child.getBoundingClientRect();
@@ -95,8 +92,8 @@ export class MapaMundiComponent implements OnInit {
         break;
     }
     let detailDataMapaMundi =document.querySelector('#DetailDataMapaMundi').getBoundingClientRect();
-    this.sedes.filter(e=>e.name==name)[0]['choords'].top = _size.top - detailDataMapaMundi.top ;
-    this.sedes.filter(e=>e.name==name)[0]['choords'].left = _size.left - detailDataMapaMundi.left ;
+    this.sedes.filter(e=>e.svgName==name)[0]['choords'].top = _size.top - detailDataMapaMundi.top ;
+    this.sedes.filter(e=>e.svgName==name)[0]['choords'].left = _size.left - detailDataMapaMundi.left ;
   
     _size = {
       top: _size.top  - websize.top,
