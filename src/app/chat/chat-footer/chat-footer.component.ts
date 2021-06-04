086ddb38947cc9b6edf9b289b2d4add19b4babbc
Cristian Danilo Motta Herrera <cdmottah@gmail.com>
2021-06-04 10:04:01 -0500

    se reduce tamaños de las imagenes

--- src/app/chat/chat-footer/chat-footer.component.ts
+++ src/app/chat/chat-footer/chat-footer.component.ts
@@ -21,11 +21,11 @@ export class ChatFooterComponent implements OnInit {
   '8KHklcgn0xg','Jegv-Ht2Qeo','LQYyau47oy4','TAWXA-ccj7I','wzraTerfxLI','tJSu7nrf8AM'
   ]
   links= [
-    {text:'Linkedin', url:'https://www.linkedin.com/company/si-silice', texturl:'www.linkedin.com/silice', image:'linkedin.jpg'},
-    {text:'Facebook', url:'https://www.facebook.com/sisilice/', texturl:'www.facebook.com/silice', image:'facebook.jpg'},
-    {text:'Instagram', url:'https://www.instagram.com/silicesi/', texturl:'www.instagram.com/Siice', image:'instagram.jpg'},
-    {text:'Twitter', url:'https://twitter.com/Si_Silice', texturl:'www.twitter.com/Si_Silice', image:'twitter.jpg'},
-    {text:'Youtube', url:'https://www.youtube.com/channel/UCI-ZhoL4V3uZorOJZD849dg', texturl:'www.youtube.com/Siice', image:'youtube.jpg'},
+    {text:'Linkedin', url:'https://www.linkedin.com/company/si-silice', texturl:'www.linkedin.com/silice', image:'Slinkedin.webp'},
+    {text:'Facebook', url:'https://www.facebook.com/sisilice/', texturl:'www.facebook.com/silice', image:'Sfacebook.webp'},
+    {text:'Instagram', url:'https://www.instagram.com/silicesi/', texturl:'www.instagram.com/Siice', image:'Sinstagram.webp'},
+    {text:'Twitter', url:'https://twitter.com/Si_Silice', texturl:'www.twitter.com/Si_Silice', image:'Stwitter.webp'},
+    {text:'Youtube', url:'https://www.youtube.com/channel/UCI-ZhoL4V3uZorOJZD849dg', texturl:'www.youtube.com/Siice', image:'Syoutube.webp'},
   ]
   sedes=[
     {image:'assets/images/sedes/flag2-españa.jpg', link:'nosotros/sedes/espana', name:'España', city:'Extremadura'},
