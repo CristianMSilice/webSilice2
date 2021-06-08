44e987a41c877ae3a263877b9929b9779b58a9fd
Cristian Danilo Motta Herrera <cdmottah@gmail.com>
2021-06-08 11:35:12 -0500

    se limpia parte del codigo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { HttpModule } from '@angular/http'
 import { BrowserModule, Title } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { Routes } from '@angular/router'
 import { ParticlesModule } from 'angular-particle'
 import { InlineSVGModule } from 'ng-inline-svg'
 import { CookieService } from 'ngx-cookie-service'
@@ -16,44 +15,14 @@ import { EncsessionService } from './chat/shared/helpers/encsession.service'
 import { ConfigFactory, ConfigService, TOKEN } from './chat/shared/services/config'
 import { ElementModule } from './element.module'
 import { SharedModule } from './shared/shared/shared.module'
-import { CasosComponent } from './web/components/casos/casos.component'
-import { ClientsComponent } from './web/components/clients/clients.component'
-import { NosotrosEquipoComponent } from './web/components/equipo/equipo.component'
-import { NosotrosInfografiaComponent } from './web/components/infografia/infografia.component'
-import { ColombiaComponent } from './web/components/locations/colombia/colombia.component'
-import { EspanaComponent } from './web/components/locations/espana/espana.component'
-import { LocationsHeaderComponent } from './web/components/locations/locations-header/locations-header.component'
-import { PanamaComponent } from './web/components/locations/panama/panama.component'
-import { MapaMundiComponent } from './web/components/mapa-mundi/mapa-mundi.component'
-import { ModalComponent } from './web/components/modal/modal.component'
-import { NosotrosPresenciaComponent } from './web/components/presencia/presencia.component'
-import { SliderComponent } from './web/components/principalSlider/slider/slider.component'
-import { SolucionesComponent } from './web/components/soluciones/soluciones.component'
 import { NosotrosSomosComponent } from './web/components/somos/somos.component'
 import { TeamComponent } from './web/components/team/team.component'
 import { NosotrosTrayectoriaComponent } from './web/components/trayectoria/trayectoria.component'
 import { FooterComponent } from './web/layout/footer/footer.component'
 import { MainHeaderComponent } from './web/layout/header/header.component'
 import { MainMenuComponent } from './web/layout/menu/menu.component'
-import { BlogDestacadosComponent } from './web/pages/blog/blog-destacados/blog-destacados.component'
-import { BlogGeneralComponent } from './web/pages/blog/blog-general/blog-general.component'
-import { BlogHeaderComponent } from './web/pages/blog/blog-header/blog-header.component'
-import { BlogPrincipalComponent } from './web/pages/blog/blog-principal/blog-principal.component'
-import { BlogVistosComponent } from './web/pages/blog/blog-vistos/blog-vistos.component'
-import { BlogComponent } from './web/pages/blog/blog.component'
-import { SingleComponent } from './web/pages/blog/single/single.component'
-import { PageCasosComponent } from './web/pages/casos/page-casos.component'
 import { ContactoComponent } from './web/pages/experiencia/contacto/contacto.component'
-import { PageColombiaComponent } from './web/pages/experiencia/sedes/page-colombia/page-colombia.component'
-import { PageEspanaComponent } from './web/pages/experiencia/sedes/page-espana/page-espana.component'
-import { PagePanamaComponent } from './web/pages/experiencia/sedes/page-panama/page-panama.component'
-import { HomeComponent } from './web/pages/home/home.component'
 import { LaboratorioComponent } from './web/pages/laboratorio/laboratorio.component'
-import { PageEquipoComponent } from './web/pages/nosotros/equipo/page-equipo.component'
-import { PagePresenciaComponent } from './web/pages/nosotros/presencia/page-presencia.component'
-import { PageSomosComponent } from './web/pages/nosotros/somos/page-somos.component'
-import { PageTrayectoriaComponent } from './web/pages/nosotros/trayectoria/page-trayectoria.component'
-import { PageSolucionesComponent } from './web/pages/soluciones/page-soluciones.component'
 import { MailService } from './web/services/mail.service'
 import { SiblingsService } from './web/services/siblings.service'
 import { WebComponent } from './web/web/web.component'
@@ -77,45 +46,15 @@ import { WebComponent } from './web/web/web.component'
   ],
   declarations: [
     AppComponent,
-    BlogComponent,
-    PageCasosComponent,
     ContactoComponent,
-    PageEquipoComponent,
     MainHeaderComponent,
     MainMenuComponent,
-    HomeComponent,
     LaboratorioComponent,
-    PageSolucionesComponent,
-    SliderComponent,
-    PageTrayectoriaComponent,
     WebComponent,
-    ModalComponent,
-    LocationsHeaderComponent,
-    PanamaComponent,
-    ColombiaComponent,
-    EspanaComponent,
-    PageEspanaComponent,
-    PageColombiaComponent,
-    PagePanamaComponent,
-    MapaMundiComponent,
-    ClientsComponent,
     NosotrosSomosComponent,
     NosotrosTrayectoriaComponent,
-    NosotrosEquipoComponent,
-    NosotrosPresenciaComponent,
-    PageSomosComponent,
-    PagePresenciaComponent,
-    NosotrosInfografiaComponent,
-    SolucionesComponent,
-    CasosComponent,
     FooterComponent,
     MainMenuComponent,
-    BlogDestacadosComponent,
-    BlogPrincipalComponent,
-    BlogGeneralComponent,
-    BlogVistosComponent,
-    BlogHeaderComponent,
-    SingleComponent,
     TeamComponent
   ],
 
